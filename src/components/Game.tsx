c4d59458a34d7cc478fa38fa78eec585d07dba0e
Matthias <matthiaspinard@gmail.com>
2022-03-13 20:37:22 +0100

    update

--- src/components/Game.tsx
+++ src/components/Game.tsx
@@ -10,13 +10,13 @@ const Game = () => {
   return (
     <>
       <h2 className="text-2xl my-5 font-semibold">{dateStr}</h2>
-      <GuessButton day={0} />
       <GuessButton day={1} />
       <GuessButton day={2} />
       <GuessButton day={3} />
       <GuessButton day={4} />
       <GuessButton day={5} />
       <GuessButton day={6} />
+      <GuessButton day={0} />
     </>
   );
 };
@@ -26,7 +26,7 @@ const GuessButton = ({ day }: { day: number }) => {
   const dispatch = useContext(GameStateDispatcherContext);
   return (
     <button
-      className={`mx-2 px-2 rounded-md bg-gray-200 hover:bg-gray-300`}
+      className={`mx-2 px-2 py-1 rounded-md bg-gray-200 hover:bg-gray-300`}
       onClick={() => dispatch({ type: "guess", guess: day })}
     >
       {t("days", { returnObjects: true })[day]}
